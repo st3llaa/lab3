c71873d1e6e5108def223f05a69849714ad98ebc
st3llaa <89151849+st3llaa@users.noreply.github.com>
2023-04-20 09:29:01 -0700

    checkpoint 1
     fixing reverse array method bugs (both functions)

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -3,20 +3,22 @@
 public class ArrayExamples {
 
   // Changes the input array to be in reversed order
-  static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+  static void reverseInPlace(int[] arr) { //only need to change up until half way bc of swapping
+    for(int i = 0; i < arr.length/2; i += 1) {
+      int temp = arr[i]; //origional didn't have a temp variable
+      arr[i] = arr[arr.length-1 - i];
+      arr[arr.length-1 - i] = temp; //swapping the two numbers
     }
   }
 
   // Returns a *new* array with all the elements of the input array in reversed
   // order
-  static int[] reversed(int[] arr) {
+  static int[] reversed(int[] arr) { 
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length -i -1];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,25 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseInPlaceBug(){
+    int[] input = {1, 2, 3, 4};
+    int[] output = {4, 3, 2, 1};
+    ArrayExamples.reverseInPlace(input);
+    assertArrayEquals(output, input);
+  }
+
+  @Test
+  public void testReversedBug(){
+    int[] input = {-1, -2, -3};
+    int[] output = {-3, -2, -1};
+    ArrayExamples.reversed(input);
+    assertArrayEquals(output, ArrayExamples.reversed(input));
   }
 }
